f822af13d8900e92a23c719663e67ff3afeeb267
Макс <maxkalimullin@gmail.com>
2021-04-22 19:10:43 +0300

    new keyboard

---
+++ docs/script.js
@@ -0,0 +1,42 @@
+const display = document.querySelector('.display');
+const clearButton = document.querySelector('.button-clear');
+const buttons = document.querySelectorAll('.button');
+
+for (const button of buttons) {
+
+    button.addEventListener('click', () => {
+        display.textContent += button.textContent;
+    });
+
+    document.addEventListener('keydown', (evt) => {
+        if (evt.key === " ") {
+            evt.preventDefault();
+        }
+
+        if (evt.key === button.textContent) {
+            button.click();
+            button.classList.add('button--clicked');
+            console.log(evt.key);
+        }
+
+        if (evt.key === "Backspace") {
+            clearButton.classList.add('button--clicked');
+            clearButton.click();
+            display.textContent = '';
+        }
+    });
+    document.addEventListener('keyup', (evt) => {
+
+        if (evt.key === button.textContent) {
+            button.classList.remove('button--clicked');
+        }
+
+        if (evt.key === "Backspace") {
+            clearButton.classList.remove('button--clicked');
+        }
+    });
+}
+
+clearButton.addEventListener('click', () => {
+    display.textContent = '';
+});
